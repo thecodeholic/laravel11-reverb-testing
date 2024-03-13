2a1e06bf38f24ac1172416efe719be003a7330bc
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2024-03-13 22:10:25 +0400

    Implement public messages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Events\MyPublicMessage;
+use App\Models\Message;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $messages = Message::get();
+    return view('welcome', compact('messages'));
+});
+
+Route::get('/message/{message}', function (string $message) {
+    $model = new Message();
+    $model->message = $message;
+    $model->save();
+    event(new MyPublicMessage($message));
+    return 'You just broadcasted a public message "' . $message . '"';
 });
