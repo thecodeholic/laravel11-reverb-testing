0e1fd1b78c4ba5e96e6ed471edee33d2b9a474ab
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2024-03-28 08:46:25 +0400

    Change public message from ShouldBroadcastNow into ShouldBroadcast

--- app/Events/MyPublicMessage.php
+++ app/Events/MyPublicMessage.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MyPublicMessage implements ShouldBroadcastNow
+class MyPublicMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
